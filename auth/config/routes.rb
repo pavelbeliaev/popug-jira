e2e9ff499b2a7e4efecb1a0a832e5f3dfbe00e83
Pavel Beliaev <pavel.beliaev@protonmail.com>
2021-11-06 08:45:32 +0700

    Auth. Scaffold accounts

--- auth/config/routes.rb
+++ auth/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   use_doorkeeper
   devise_for :accounts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :accounts
 end
